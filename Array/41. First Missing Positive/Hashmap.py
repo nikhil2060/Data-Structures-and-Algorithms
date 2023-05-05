624ab96139ec9331b98a0956222caf53fa3982b1
nikhil2060 <102829906+nikhil2060@users.noreply.github.com>
2023-05-06 00:34:07 +0530

    6Map

---
+++ Array/41. First Missing Positive/Hashmap.py
@@ -0,0 +1,24 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        nums.sort();
+        
+        hmap={}
+        
+        for i in nums:
+            
+            if i not in hmap:
+                hmap[i]=i
+            else:
+                hmap[i]+=1
+                
+        i=1
+        
+        while i:
+        
+            if not i in hmap:
+                return i
+            
+            i+=1
+        
+        return 0;
