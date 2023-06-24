9a826c6625995cdf1a24d299107caf3d43660745
nikhil2060 <kumarnikhil2060@gmail.com>
2023-06-24 17:07:20 +0530

    24jun

---
+++ Array/485. Max Consecutive Ones/sol1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int maxi=0;
+        bool flag=true;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                flag=true;
+            }
+            else
+            {
+                flag=false;
+            }
+            
+            if(flag==true)
+            {
+                count++;
+                maxi=max(count,maxi);
+            }
+            else
+            {
+                count=0;
+            }
+        }
+        
+        return maxi;
+    }
+};
