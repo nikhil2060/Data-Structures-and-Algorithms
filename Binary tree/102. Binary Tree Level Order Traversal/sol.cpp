b65cf347e74194ea1a23da338d234a814c0d88b6
nikhil2060 <kumarnikhil2060@gmail.com>
2024-05-21 10:58:52 +0530

    21/0

---
+++ Binary tree/102. Binary Tree Level Order Traversal/sol.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+
+        vector<int> temp;
+
+        q.push(root);
+
+        while (!q.empty()) {
+            int cap = q.size();
+
+            for (int i = cap; i >0; i--) {
+                // cout << i << endl;
+                if (q.empty())
+                    break;
+                TreeNode* currentNode = q.front();
+                q.pop();
+                if (currentNode == NULL)
+                    continue;
+                temp.push_back(currentNode->val);
+                q.push(currentNode->left);
+                q.push(currentNode->right);
+            }
+
+            if (!temp.empty()) {
+                ans.push_back(temp);
+            }
+
+            for(int i=0;i<temp.size();i++)
+            {
+                cout<<temp[i]<<',';
+            }
+            temp.clear();
+        }
+
+        return ans;
+    }
+};
